dbc43b9f87c872ed10c5eb774ca441e9217f9ad1
wh0-cares <careswh4@gmail.com>
2020-10-02 17:03:13 -0700

    yeah im back o working on this now

--- wh0bot.py
+++ wh0bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from pathlib import Path 
 import json
 
+
+#Basic setup
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
 
@@ -17,6 +19,19 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
      await ctx.send(f'Hello Yes, what do you want? Its only been {round(client.latency * 1000)}ms')
+
+@client.command()
+async def say(ctx,arg):
+    await ctx.send(f"{arg}")
+
+#Hardcoded Moderation
+@client.command()
+async def clear(ctx,amount=1):
+    await ctx.channel.purge(limit=amount+1)
+    if (amount==1):
+        await ctx.send(f"successfully cleared {amount} message") 
+    else:
+        await ctx.send(f"successfully cleared {amount} messages") 
 
 
 
